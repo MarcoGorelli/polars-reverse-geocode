91b352e4c9d455b877b1ca88de1316a6ddfeb231
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-02-20 16:47:02 +0000

    cln

--- run.py
+++
@@ -1,9 +0,0 @@
-import polars as pl
-from polars_reverse_geocode import pig_latinnify
-
-df = pl.DataFrame({
-    'english': ['this', 'is', 'not', 'pig', 'latin'],
-})
-result = df.with_columns(pig_latin = pig_latinnify('english'))
-print(result)
-
