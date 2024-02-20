ee8ac5d41d213a3d732661a75148f73651216ad0
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-02-20 17:09:42 +0000

    publish

---
+++ tests/test_main.py
@@ -0,0 +1,17 @@
+import polars as pl
+from polars.testing import assert_frame_equal
+
+from polars_reverse_geocode import reverse_geocode
+
+
+def test_main() -> None:
+    df = pl.DataFrame({"lat": [37.7749, 51.01, 52.5], "lon": [-122.4194, -3.9, -0.91]})
+    result = df.with_columns(city=reverse_geocode("lat", "lon"))
+    expected = pl.DataFrame(
+        {
+            "lat": [37.7749, 51.01, 52.5],
+            "lon": [-122.4194, -3.9, -0.91],
+            "city": ["San Francisco", "South Molton", "Market Harborough"],
+        }
+    )
+    assert_frame_equal(result, expected)
