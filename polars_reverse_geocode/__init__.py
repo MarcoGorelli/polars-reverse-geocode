17b1dc816913fe60c1b4c27316a5243ea06b7308
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-03-26 11:02:11 +0000

    0.20.16 compat

--- polars_reverse_geocode/__init__.py
+++ polars_reverse_geocode/__init__.py
@@ -2,21 +2,27 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from pathlib import Path
 import polars as pl
 from polars.utils.udfs import _get_shared_lib_location
 
-from polars_reverse_geocode.utils import parse_into_expr
+from polars_reverse_geocode.utils import parse_into_expr, register_plugin, parse_version
 
 __version__ = "0.1.9"
 
 if TYPE_CHECKING:
     from polars.type_aliases import IntoExpr
 
-lib = _get_shared_lib_location(__file__)
+if parse_version(pl.__version__) < parse_version("0.20.16"):
+    from polars.utils.udfs import _get_shared_lib_location
+
+    lib: str | Path = _get_shared_lib_location(__file__)
+else:
+    lib = Path(__file__).parent
 
 
 def reverse_geocode(lat: IntoExpr, long: IntoExpr) -> pl.Expr:
     lat = parse_into_expr(lat)
-    return lat.register_plugin(
-        lib=lib, symbol="reverse_geocode", is_elementwise=True, args=[long]
+    return register_plugin(
+        lib=lib, symbol="reverse_geocode", is_elementwise=True, args=[lat, long]
     )
