f945a15b4bae5c3ff19a7e12bf559c8f3ef95602
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-06-12 09:44:27 +0100

    polars 1.0 compat

--- t.py
+++ t.py
@@ -1,9 +1,9 @@
 import polars as pl
 
-from polars_reverse_geocode import h3
+from polars_reverse_geocode import find_closest_city
 
 df = pl.DataFrame({
     'lat': [37.7749, 51.01, 52.5],
     'lon': [-122.4194, -3.9, -.91]
 })
-print(df.with_columns(h3=h3('lat', 'lon')))
+print(df.with_columns(city = find_closest_city('lat', 'lon')))
