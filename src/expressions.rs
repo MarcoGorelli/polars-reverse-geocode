dcee7547d2db7c8eed2d50085aa76eac26d22bf8
Bohdan Kholodenko <bohdan.kholodenko@gmail.com>
2024-12-07 16:34:48 +0200

    Switch back to 'binary_elementwise_into_string_amortized' for cleaner implementation

--- src/expressions.rs
+++ src/expressions.rs
@@ -1,7 +1,6 @@
 #![allow(clippy::unused_unit)]
 use polars::prelude::*;
-use polars_arrow::array::MutablePlString;
-use polars_core::utils::align_chunks_binary;
+use polars_core::prelude::arity::binary_elementwise_into_string_amortized;
 use pyo3_polars::derive::polars_expr;
 use std::fmt::Write;
 
@@ -22,35 +21,15 @@ fn reverse_geocode_chunks(
     let rhs = inputs[1].f64()?;
     let geocoder = ReverseGeocoder::new();
 
-    let (lhs, rhs) = align_chunks_binary(lhs, rhs);
-    let chunks = lhs
-        .downcast_iter()
-        .zip(rhs.downcast_iter())
-        .map(|(lhs_arr, rhs_arr)| {
-            let mut buf = String::new();
-            let mut mutarr = MutablePlString::with_capacity(lhs_arr.len());
-
-            for (lhs_opt_val, rhs_opt_val) in lhs_arr.iter().zip(rhs_arr.iter()) {
-                match (lhs_opt_val, rhs_opt_val) {
-                    (Some(lhs_val), Some(rhs_val)) => {
-                        let record = &geocoder.search((*lhs_val, *rhs_val)).record;
-                        let res: &String = match location_level {
-                            LocationLevel::City => &record.name,
-                            LocationLevel::State => &record.admin1,
-                            LocationLevel::Country => &record.cc,
-                        };
-                        buf.clear();
-                        write!(buf, "{res}").unwrap();
-                        mutarr.push(Some(&buf))
-                    }
-                    _ => mutarr.push_null(),
-                }
-            }
-
-            mutarr.freeze().boxed()
-        })
-        .collect();
-    let out: StringChunked = unsafe { ChunkedArray::from_chunks(PlSmallStr::EMPTY, chunks) };
+    let out = binary_elementwise_into_string_amortized(lhs, rhs, |lat, lon, buf| {
+        let record = geocoder.search((lat, lon)).record;
+        let res: &String = match location_level {
+            LocationLevel::City => &record.name,
+            LocationLevel::State => &record.admin1,
+            LocationLevel::Country => &record.cc,
+        };
+        write!(buf, "{}", res).unwrap();
+    });
     Ok(out.into_series())
 }
 
